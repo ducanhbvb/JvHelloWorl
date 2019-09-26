b0ac7227612c2693daf5da742cbf4790bdd95edf
ducanhbvb <ducanhnguyen.bvb@gmail.com>
2019-09-26 15:54:40 +0700

    nop bai 26/09/2019

---
+++ src/HelloWorl.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HelloWorl{
+public static void main(String[] svg){
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name = scanner.nextLine();
+
+    System.out.println("Hello: " + name);
+}
+}
